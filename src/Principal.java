318b7fc7efa2cbb045f098975a8da5c589afde16
Esteban Ruiz Novoa <esteban.ruiz@uniminuto.edu.co>
2023-02-07 19:01:35 -0500

    ajuste salida por consola

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,8 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Bienvenidos a la clase CP3");
 
+        System.out.println("EN ESTA CLASE VOY A APRENDER GIT");
+
     }
 
 }
